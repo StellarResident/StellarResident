aecbaa280fcbf4c59cb297faf46cdd20ae07ba96
StellarResident <stellarresident@gmail.com>
2023-04-16 23:28:01 +0900

    kkyukkyu

---
+++ Python/Study/List.py
@@ -0,0 +1,7 @@
+#List are used to store multiple items in a single variable.
+
+#Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple,Set, and Dictionary, all with different qualities and usage.
+a = ['Sigma', 'Horizon', 7, 9]
+print(a)
+
+#List +
