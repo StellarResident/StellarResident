eb87a48c88e5553a2feb3126fbfa70d7f15ea0ad
StellarResident <stellarresident@gmail.com>
2023-04-16 22:28:21 +0900

    kkyukkyu

--- Python/BMI.py
+++ Python/BMI.py
@@ -2,16 +2,16 @@ Height = float(input(" Enter your height in centimeters : "))
 Weight = float(input(" Enter your height in Kg : "))
 Height = Height / 100
 BMI = Weight / (Height * Height)
-print ("Your BMI is : " , BMI)
+print (" Your BMI is : " , BMI)
 if (BMI > 0) :
             if (BMI <= 18.5) :
-                    print("Underweight")
+                    print(" Underweight ")
             elif (BMI <= 23) :
-                    print("Healthy")
+                    print(" Healthy ")
             elif (BMI <= 25) :
-                    print("Overweight")
+                    print(" Overweight ")
             elif (BMI <= 30) :
-                    print("Mild obesity")
+                    print(" Mild obesity ")
             else : 
-                    print("Excessive obesity")
+                    print(" Excessive obesity ")
         
