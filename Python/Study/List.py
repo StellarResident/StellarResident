1bb581cd557998284185e85679fa6157a0baa6bf
StellarResident <stellarresident@gmail.com>
2023-04-17 22:59:06 +0900

    kkyukkyu

--- Python/Study/List.py
+++ Python/Study/List.py
@@ -5,3 +5,47 @@ a = ['Sigma', 'Horizon', 7, 9]
 print(a)
 
 #List +
+b = [' Destiny ', 666]
+c = [' Look ', 875]
+print(b + c)
+
+#List *
+d = b * 3
+print(d) 
+e = b * 0
+print(e)
+
+#list lenth = 8
+g = [3, 6, 9, 12, ' Sky ', ' Ground ', 7, 27]
+print(g)
+#Index
+print(g[0])
+print(g[1])
+print(g[3])
+print(g[7])
+
+#Reverse index
+print(g[-1])
+print(g[-3])
+
+#List value retouch
+print(g)
+g[0] = 1
+print(g)
+g[3] = ' Titan '
+print(g)
+g[-3] = ' Sea '
+print(g)
+
+#List slicing
+print(g[0:4])
+print(g[1:])
+print(g[:5])
+
+#List half
+z = int(len(g) / 2)
+print(g[:z])
+print(g[-2:])
+print(g[-5:-1])
+print(g[:-6])
+print(g[:])
